ec301fa3b34ce09e7c9fcfa8f3875ca3756a87bf
Bunyod2009 <bunyodshavkatov362@gmail.com>
2024-09-07 14:04:51 +0300

    js

---
+++ script.js
@@ -0,0 +1,23 @@
+var name = prompt ("Ismingizni kiriting")
+console.log(`Sizning ismngiz ${name}`)
+
+var age = +prompt("Yoshingizni kiritinig")
+console.log(`Sizning yoshingiz: ${age}`)
+
+console.log(`6+3=`+(6+3));
+var misol = +prompt("6+3=?")
+console.log(`Sizning javobingiz: ${misol}`);
+
+console.log(`25-3=`+(25-3));
+var misol = +prompt("25-3=?")
+console.log(`Sizning javobingiz: ${misol}`);
+
+console.log(`10/2=`+(10/2));
+var misol = +prompt("10/2=?")
+console.log(`Sizning javobingiz: ${misol}`);
+
+console.log(`10%3=`+(10%3));
+var misol = +prompt("10%3=?")
+console.log(`Sizning javobingiz: ${misol}`);
+
+alert(`Rahmat ${name}`)
